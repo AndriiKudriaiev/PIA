c46172508a754753f47f2874516a7c7405e54024
Andrii Kudriaiev <andrii.kudriaiev@icloud.com>
2025-04-15 22:12:27 +0200

    Added basic logging system. Needs to be adjusted.

--- app.py
+++ app.py
@@ -1,38 +1,59 @@
 import logging
 import pdfplumber
 import os
+import pytz
+import platform
+from pathlib import Path
 from prompt_toolkit.shortcuts import radiolist_dialog, message_dialog
+from datetime import datetime
 
 logging.getLogger("pdfminer").setLevel(logging.ERROR)
 
 def get_table_data(file, page_number=0):
     with pdfplumber.open(file) as pdf:
-        page = pdf.pages[page_number]
-        table = page.extract_table()
+        page=pdf.pages[page_number]
+        table=page.extract_table()
     return table
 
 def select_source_file_screen():
     if not os.path.exists("Input"):
         print("Can't locate 'Input' folder")
         return None
-    elif len(os.listdir("Input")) == 0:
-        result = message_dialog(
-                title = "Error",
-                text = "There're no files inside Input folder.",
+    elif len(os.listdir("Input"))==0:
+        result=message_dialog(
+                title="Error",
+                text="There're no files inside Input folder.",
                 ).run()
         return result
     else:
-        result = radiolist_dialog(
-            title = "Select input file:",
-            values = [(i, i) for i in os.listdir("Input") if i.endswith(".pdf")],
+        result=radiolist_dialog(
+            title="Select input file:",
+            values=[(i, i) for i in os.listdir("Input") if i.endswith(".pdf")],
     ).run()
     return result
 
 def main():
     try:
-        input_file = select_source_file_screen()
+        log_dir=Path.cwd() / "log"
+        log_dir.mkdir(parents=True, exist_ok=True)
+        timezone=pytz.timezone("Europe/Berlin")
+        time_now=datetime.now(timezone)
+        log_file=log_dir / f"{str(time_now)[:-22]}_{str(time_now)[11:-16]}.log"
+        logger=logging.getLogger("app_logger")
+        logger.setLevel(logging.DEBUG)
+
+        file_handler=logging.FileHandler(log_file, encoding="utf-8")
+
+        formatter=logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+
+        logger.info("Session has started.")
+        logger.debug(f"{type(time_now)}")
+        input_file=select_source_file_screen()
         index=0
-        if input_file == None:
+        if input_file==None:
             print("Exiting...")
         else:
             print(f"Selected file: {input_file}\n")
@@ -40,7 +61,8 @@ def main():
             for row in table_data[1:]:
                 index+=1
                 print(f"[{index}]",row[1:-8])
-            input("\nPress Enter to continue with printing.")
+                
+            input("\nPress ENTER to continue with printing.")
     except:
         print("An error occurred. Try reading the documentation.")
         return
